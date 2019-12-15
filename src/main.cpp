277f2e3a161576d82a5b74ed735b421c34bc2857
Blickfeldkurier <Blickfeldkurier@posteo.de>
2019-12-15 11:28:30 +0100

    Comments added

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,24 @@
 #include <sys/inotify.h>
 
 #include "log.h"
+
+//http_upload is from the google_breakpad library.
+//since it is not installed in Path, it was copied 
+//and modified to fit the Programm structure
 #include "http_upload.h"
 
+//Wrapper arround http_upload.h
 using namespace google_breakpad;
-
 namespace {
+    /**
+     * Upload a Minidumpfile to given Url.
+     * Takes a Filepath and uploads it to the given url.
+     * On Error it returns False and sets the string given by error.
+     * @param path Path to the file we want to upload
+     * @param url Url to upload to
+     * @param error Contains the error text
+     * @return bool False on error
+     **/
     bool uploadMinidump(std::string &path, std::string &url, std::string *error) {
         std::map<string, string> parameters;
         std::map<string, string> files;
@@ -33,6 +46,13 @@ namespace {
     }
 }
 
+/**
+ * Delete a File if the upload was succssesfull
+ * @param path Path to the file we want to be deleted
+ * @param noError Return value of the File upload. True == Upload succssesfull
+ * @param errmsg Error String from upload, might contain the reason for failure
+ * @param log Pointer to the Log object we want to write the error messages to. 
+ */ 
 void deleteFile(std::string &path, bool noError, std::string *errmsg, Log *log){
     if(noError == false){
         log->print("Could not upload " + path + ": " + *errmsg + "\n");
@@ -57,6 +77,7 @@ void printHelp(){
 }
 
 int main(int argc, char *argv[]){
+    //Structure for long_options
     static struct option long_options[] = {
 		{"contains", required_argument, 0, 'c'},
 		{"debug", no_argument,0, 'd'},
@@ -66,6 +87,7 @@ int main(int argc, char *argv[]){
         {0, 0, 0, 0}
     };
 
+    //Option Parsing
     int opt = 0;
     int option_index = 0;
 	bool isDebug = false;
@@ -76,20 +98,20 @@ int main(int argc, char *argv[]){
                    long_options, &option_index )) != -1) 
     {
         switch(opt){
-			case 'c':{
+			case 'c':{//Pattern for filetest
 				contains = std::string(optarg);
 			}
-			case 'd':{
+			case 'd':{//Flag for Debug mode
 				isDebug = true;
 			}break;
-            case 'h':{
+            case 'h':{//Print help
                 printHelp();
                 exit(0);
             }break;
-            case 'p':{
+            case 'p':{//Path to Folder we want to watch
                 inotify_path = std::string(optarg);
             }break;
-            case 'u':{
+            case 'u':{//Upload Url
                 url = std::string(optarg);
             }break;
             case '?':{
@@ -100,12 +122,17 @@ int main(int argc, char *argv[]){
     }
 
 	if(isDebug == false){
-		daemon(0,0);	
+		if(daemon(0,0) != 0){
+            std::string failstr = std::string(strerror(errno));
+            std::cout << ("Could not daemonize:\n" + failstr);
+            exit(1);
+        }
 	}
 	Log *log = new Log(isDebug);
     log->print("Path to Inode: " + inotify_path + "\n", "");
     log->print("Upload to: " + url + "\n", "");
 
+    //Setup Inotify
     int ifd = inotify_init();
     if(ifd == -1){
         std::string failstr = std::string(strerror(errno));
@@ -119,6 +146,7 @@ int main(int argc, char *argv[]){
         return (-1);
     }
     
+    //Mainloop
     bool running = true;
     while(running){
         const int BUF_LEN = 4096;
@@ -130,6 +158,7 @@ int main(int argc, char *argv[]){
             log->print("Could read from  inotify fd:\n" + failstr);
             continue;
         }
+        //Process Inotify Events
         event = reinterpret_cast<struct inotify_event *>(buffer);
         while(event != nullptr){
             if((event->mask & IN_CLOSE_WRITE) && (event->len > 0)){
@@ -148,7 +177,7 @@ int main(int argc, char *argv[]){
                 }
                 delete(error);
 			}
-            /* Move to next struct */
+            //Move to next struct
             len -= sizeof(*event) + event->len;
             if (len > 0)
                 event =  event + sizeof(event) + event->len;
@@ -157,6 +186,7 @@ int main(int argc, char *argv[]){
             }
     }
 
+    //Celeanup
     if(inotify_rm_watch(ifd, wfd) == -1){
         std::string failstr = std::string(strerror(errno));
         log->print("Could not remove watch from inotify:\n" + failstr);
